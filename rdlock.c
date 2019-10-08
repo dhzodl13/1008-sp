13a5450a9add8cbba940d2f60de0299656260c55
dhzodl13 <54874684+dhzodl13@users.noreply.github.com>
2019-10-08 11:34:20 +0900

    Add files via upload

--- rdlock.c
+++ rdlock.c
@@ -2,7 +2,7 @@
 #include <fcntl.h>
 #include "student.h"
 
-int main(int argc,char *argv[])
+int main(int argc, char *argv[])
 {
 	int fd, id;
 	struct student record;
@@ -22,19 +22,19 @@ int main(int argc,char *argv[])
 	while (scanf("%d",&id) == 1){
 		lock.l_type = F_RDLCK;
 		lock.l_whence = SEEK_SET;
-		lock.l_start = (id-START_ID)sizeof(record);
+		lock.l_start = (id-START_ID)*sizeof(record);
 		lock.l_len = sizeof(record);
-		if(fcntl(fd.f_SETLKW, &lock) == -1){
+		if(fcntl(fd,F_SETLKW, &lock) == -1){
 			perror(argv[1]);
 			exit(3);
 		}
 	
 		lseek(fd,(id-START_ID)*sizeof(record),SEEK_SET);
 		if((read(fd,(char *) &record,sizeof(record)) > 0) && (record.id !=0))
-			printf("이름:%s\t 학번:%d\t 점수:%d\m",record.name,record.id,record.score);
+			printf("이름:%s\t 학번:%d\t 점수:%d\n",record.name,record.id,record.score);
 		else printf("레코드 %d 없음\n",id);
 
-		lock.l type = F_UNLCK;
+		lock.l_type = F_UNLCK;
 		fcntl(fd, F_SETLK, &lock);
 		printf("\n검색할 학생의 학번 입력:");
 	}
